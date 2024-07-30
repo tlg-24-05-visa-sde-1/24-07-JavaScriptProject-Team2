5166a66f3b3918844e5cb4cfbda125e0899b80cf
Jonathan Reyes <jonathanr@tlgcohort.com>
2024-07-30 11:28:55 -0500

    navigation bar

---
+++ script.js
@@ -0,0 +1,25 @@
+
+/*------------- LOGIN/SIGNUP NAVIGATION ------------*/
+document
+  .getElementById("login-signup-container")
+  .addEventListener("click", handleLoginSignup);
+
+//Login/Signup Event Handler
+function handleLoginSignup(event) {
+    const homeScreen = document.getElementById('home-screen');
+    homeScreen.
+   
+  if (event.target.classList.contains("signup-button")) {
+    signupEvent;
+  } else if (event.target.classList.contains("login-button")) {
+    loginEvent;
+  }
+}
+
+function signupEvent() {
+  //TODO:CODE For Signup
+}
+
+function loginEvent() {
+  //TODO:Code for Login
+}
